e90c862988fcbc097f13b5df6ed56979e81cfcdc
nabiladigo <nabiladigo91@gmail.com>
2022-08-27 17:33:59 -0400

    done with post folder

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns =[
-    path('', views.Home.as_view(), name="home"),
+    path('posts/', views.PostList.as_view(), name="post"),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name="post_detail"),
+    path('posts/<int:pk>/update', views.PostUpdate.as_view(), name= "post_update"),
+    path('posts/<int:pk>/delete', views.PostDelete.as_view(), name="post_delete"),
+    path('posts/new/', views.PostCreate.as_view(), name= "post_create"),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,58 @@
-from django.shortcuts import render
-from django.views import View 
+# from django.shortcuts import render
+# from django.views import View 
 from django.views.generic.base import TemplateView
-from django.http import HttpResponse
+from .models import Post
+
+from django.views.generic import  DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
+# from django.http import HttpResponse
 
 
 # Create your views here.
 
 
-class Home(TemplateView):
-    template_name = "home.html"
+class PostList(TemplateView):
+   
+    template_name = "post.html"
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        name= self.request.GET.get("name")
+
+        if name != None:
+            context["posts"] = Post.objects.filter(name_icontains=name)
+            context["header"]= f"Searching for {name}"
+        else:
+            context["posts"] = Post.objects.all()
+            context["header"] = "posts"
+        return context
+
+
+
+class PostDetail(DetailView):
+    model= Post
+    template_name = "post_detail.html"
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'author', 'body']
+    template_name = "post_update.html"
+
+    def get_success_url(self):
+        return reverse( 'post_detail', kwargs={'pk': self.object.pk})
     
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = "/posts/"
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields =['title', 'author', 'body']
+    template_name = "post_create.html"
+
+    def get_succes_url(self):
+        return reverse('post_detail', kwargs = {'pk':self.object.pk})
